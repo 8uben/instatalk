3f3b2bde469a38b3f48bafb8e4e4ba5cc384ee12
Vladislav Fedyaev <vladislav.fdv@mail.ru>
2022-03-07 23:55:42 +0300

    set reddis's global variable

--- app/channels/appearance_channel.rb
+++ app/channels/appearance_channel.rb
@@ -1,16 +1,15 @@
 class AppearanceChannel < ApplicationCable::Channel
   def subscribed
-    @redis = Redis.new
     @list_name = 'current_users_nicknames'
 
     stream_from 'appearance'
 
-    @redis.rpush(@list_name, current_user.nickname)
+    $redis.rpush(@list_name, current_user.nickname)
     appear
   end
 
   def unsubscribed
-    @redis.lrem(@list_name, 1, current_user.nickname)
+    $redis.lrem(@list_name, 1, current_user.nickname)
     appear
   end
 
@@ -22,7 +21,7 @@ class AppearanceChannel < ApplicationCable::Channel
   private
 
   def nicknames_list
-    @redis.lrange(@list_name, 0, -1)
+    $redis.lrange(@list_name, 0, -1)
   end
 
   def broadcast
